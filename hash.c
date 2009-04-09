661cacdbf5a2481e82288b53e8d1ed0e4f573773
Emil Renner Berthing <esmil@mailme.dk>
2009-04-09 10:32:16 +0200

    update copyright messages

--- hash.c
+++ hash.c
@@ -1,6 +1,6 @@
 /*
 This file is part of mktorrent
-Copyright (C) 2007  Emil Renner Berthing
+Copyright (C) 2007, 2009 Emil Renner Berthing
 
 mktorrent is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
