334c42a8a31cbaefc994cfa8e951bda7e2576301
uno20001 <regisztralo111@gmail.com>
2020-04-19 20:09:07 +0200

    fix parenthesis position

--- msg.c
+++ msg.c
@@ -24,7 +24,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #include "msg.h"
 
 
-void fatal(const char *format, ...) {
+void fatal(const char *format, ...)
+{
 
 	va_list args;
 	va_start(args, format);
