f04ea681dda589fdcd67038b1a3377f6aaa29b6c
Jeremy Harmon <jeremy.harmon@zoho.com>
2021-01-24 17:08:26 -0500

    Moved verbose message out of loop

--- ftw.c
+++ ftw.c
@@ -202,15 +202,16 @@ EXPORT int file_tree_walk(const char *dirname, unsigned int nfds,
 			LL_FOR(exclude_node, m->exclude_list) {
 				const char *exclude_pattern = LL_DATA(exclude_node);
 				if (fnmatch(exclude_pattern, de->d_name, 0) != FNM_NOMATCH) {
-					if (m->verbose)
-						printf("skipping %s\n", de->d_name);
 					should_skip = true;
 					break;
 				}
 			}
 			
-			if (should_skip)
+			if (should_skip) {
+				if (m->verbose)
+					printf("skipping %s\n", de->d_name);
 				continue;
+			}
 			
 			end = path + ds->length + 1;
 			p = de->d_name;
