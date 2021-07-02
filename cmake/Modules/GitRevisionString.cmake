b6790be6f612e15c560259794a78efe9ce44b9d3
FranciscoPombal <FranciscoPombal@users.noreply.github.com>
2021-07-02 17:06:51 +0100

    CMake: refactor argument parsing and passing in functions and quote variables that are not meant to be expanded into lists

--- cmake/Modules/GitRevisionString.cmake
+++ cmake/Modules/GitRevisionString.cmake
@@ -1,40 +1,49 @@
 # Function to append git revision information to a version string
 # If there is no git repository, a FATAL_ERROR is raised
-# The result is returned in the variable specified by _rev_version_output
+# The result is returned in the variable specified by OUT_VAR
 
-function(make_git_revision_string _base_version _rev_version_output)
+function(make_git_revision_string)
+    set(options "")
+    set(oneValueArgs BASE_VERSION OUT_VAR)
+    set(multiValueArgs "")
 
-    set(_version "${_base_version}")
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        ARG
+        "${options}" "${oneValueArgs}" "${multiValueArgs}"
+    )
+
+    set(_version "${ARG_BASE_VERSION}")
 
     execute_process(COMMAND "git" "rev-parse" "--short" "HEAD"
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
         OUTPUT_VARIABLE GIT_REV
         ERROR_QUIET
     )
 
-    if (NOT ("${GIT_REV}" STREQUAL ""))
+    if(NOT ("${GIT_REV}" STREQUAL ""))
         execute_process(
             COMMAND "git" "diff" "--quiet" "--exit-code"
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
             RESULTS_VARIABLE IS_WORKING_DIR_DIRTY)
         execute_process(
             COMMAND "git" "describe" "--exact-match" "--tags"
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
             OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
         execute_process(
             COMMAND "git" "rev-parse" "--abbrev-ref" "HEAD"
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
             OUTPUT_VARIABLE GIT_BRANCH)
         string(STRIP "${GIT_REV}" GIT_REV)
         string(SUBSTRING "${GIT_REV}" 0 7 GIT_REV)
         set(GIT_DIFF "")
-        if (IS_WORKING_DIR_DIRTY)
+        if(IS_WORKING_DIR_DIRTY)
             set(GIT_DIFF "~dirty")
         endif()
         string(STRIP "${GIT_TAG}" GIT_TAG)
         string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
 
-        set(_version "${_base_version}-${GIT_REV}${GIT_DIFF} (branch: ${GIT_BRANCH}")
+        set(_version "${ARG_BASE_VERSION}-${GIT_REV}${GIT_DIFF} (branch: ${GIT_BRANCH}")
 
         if(NOT ("${GIT_TAG}" STREQUAL ""))
             string(APPEND _version " tag: ${GIT_TAG})")
@@ -42,9 +51,9 @@ function(make_git_revision_string _base_version _rev_version_output)
             string(APPEND _version ")")
         endif()
     else()
-        message(WARNING "Could not get git revision information, falling back to ${_base_version}")
+        message(WARNING "Could not get git revision information, falling back to ${ARG_BASE_VERSION}")
     endif()
 
-    set(${_rev_version_output} ${_version} PARENT_SCOPE)
+    set("${ARG_OUT_VAR}" "${_version}" PARENT_SCOPE)
 
 endfunction()
--- cmake/Modules/OptionUtils.cmake
+++ cmake/Modules/OptionUtils.cmake
@@ -6,44 +6,80 @@
 # Functions for coupling add_feature_info() or CMAKE_DEPENDENT_OPTION() and option()
 #
 
-function(feature_option_setup _name _description _default)
-    string(CONCAT _desc "${_description} (default: ${_default})")
-    option("${_name}" "${_desc}" "${_default}")
-    add_feature_info("${_name}" "${_name}" "${_desc}")
+function(feature_option_setup)
+    set(options "")
+    set(oneValueArgs NAME DESCRIPTION DEFAULT)
+    set(multiValueArgs "")
+
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        ARG
+        "${options}" "${oneValueArgs}" "${multiValueArgs}"
+    )
+
+    set(_desc "${ARG_DESCRIPTION} (default: ${ARG_DEFAULT})")
+    option("${ARG_NAME}" "${_desc}" "${ARG_DEFAULT}")
+    add_feature_info("${ARG_NAME}" "${ARG_NAME}" "${_desc}")
 endfunction()
 
 include(CMakeDependentOption)
-function(feature_option_dependent_setup _name _description _default_opt _dependency _default_dep_not_sat)
-    string(CONCAT _desc "${_description} (default: ${_default_opt}; depends on condition: ${_dependency})")
-    CMAKE_DEPENDENT_OPTION("${_name}" "${_desc}" "${_default_opt}" "${_dependency}" "${_default_dep_not_sat}")
-    add_feature_info("${_name}" "${_name}" "${_desc}")
+function(feature_option_dependent_setup ARG_NAME ARG_DESCRIPTION ARG_DEFAULT ARG_DEPENDS ARG_INIT)
+    set(options "")
+    set(oneValueArgs NAME DESCRIPTION DEFAULT DEPENDS INIT)
+    set(multiValueArgs "")
+
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        ARG
+        "${options}" "${oneValueArgs}" "${multiValueArgs}"
+    )
+
+    set(_desc "${ARG_DESCRIPTION} (default: ${ARG_DEFAULT}; depends on condition: ${ARG_DEPENDS})")
+    CMAKE_DEPENDENT_OPTION("${ARG_NAME}" "${_desc}" "${ARG_DEFAULT}" "${ARG_DEPENDS}" "${ARG_INIT}")
+    add_feature_info("${ARG_NAME}" "${ARG_NAME}" "${_desc}")
 endfunction()
 
 #
 # Functions for defining tunable variables and store and display their help text
 #
 
-# Define a tunable variable and append its help text to a list of help entries given by _tunables_help_list_name
-function(tunable_setup _name _default _description _tunables_help_list_name)
+# Define a tunable variable and append its help text to a list of help entries given by LIST
+function(feature_tunable_setup)
+    set(options "")
+    set(oneValueArgs NAME DESCRIPTION DEFAULT LIST)
+    set(multiValueArgs "")
 
-    set("${_name}" "${_default}" CACHE STRING "${_description}")
-    set(_tunable_help_text "${_name}: ${_description} (default: ${_default})")
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        ARG
+        "${options}" "${oneValueArgs}" "${multiValueArgs}"
+    )
 
-    list(APPEND ${_tunables_help_list_name} ${_tunable_help_text})
-    set(${_tunables_help_list_name} ${${_tunables_help_list_name}} PARENT_SCOPE)
+    set(_tunable_help_text "${ARG_NAME}: ${ARG_DESCRIPTION} (default: ${ARG_DEFAULT})")
+    set("${ARG_NAME}" "${ARG_DEFAULT}" CACHE STRING "${ARG_DESCRIPTION}")
 
+    list(APPEND "${ARG_LIST}" "${_tunable_help_text}")
+    set("${ARG_LIST}" "${${ARG_LIST}}" PARENT_SCOPE)
 endfunction()
 
-# Print all tunable help entries stored in the list given by _tunables_help_list_name
-function(print_tunables _tunables_help_list_name)
+# Print all tunable features help entries stored in the list given by LIST
+function(print_tunables)
+    set(options "")
+    set(oneValueArgs LIST)
+    set(multiValueArgs "")
+
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        ARG
+        "${options}" "${oneValueArgs}" "${multiValueArgs}"
+    )
 
     # parent-scope list remains unchanged after these operations
-    list(TRANSFORM ${_tunables_help_list_name} APPEND "\n" AT -1)
-    list(TRANSFORM ${_tunables_help_list_name} PREPEND "* ")
-    list(PREPEND ${_tunables_help_list_name} "Tunables:\n")
+    list(TRANSFORM "${ARG_LIST}" APPEND "\n" AT -1)
+    list(TRANSFORM "${ARG_LIST}" PREPEND "* ")
+    list(PREPEND "${ARG_LIST}" "Tunables:\n")
 
-    foreach(_element IN LISTS ${_tunables_help_list_name})
+    foreach(_element IN LISTS "${ARG_LIST}")
         message(STATUS "${_element}")
     endforeach()
-
 endfunction()
