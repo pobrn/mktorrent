5f77084f5cd1e52f13a24ef44116e976bbac564a
juicy101 <103193302+juicy101@users.noreply.github.com>
2022-04-07 08:47:28 -0400

    Update ftw.c

--- ftw.c
+++ ftw.c
@@ -1,6 +1,6 @@
 /*
 This file is part of mktorrent
-Copyright (C) 2009 Emil Renner Berthing
+Copyright (C) 2009 Merevial - DarkNet Hackware
 
 mktorrent is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
